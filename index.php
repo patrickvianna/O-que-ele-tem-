1cd7a2d033c3bd6db7bde36b5fd6ddc58a285fa1
Patrick Vianna <patrickviannapblv@gmail.com>
2017-03-14 23:24:31 +0000

    atualizando os paranue

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
+    <meta charset="UTF-8"/>
     <link rel="stylesheet" type="text/css" href="www/css/grid.css" />
     <link rel="stylesheet" type="text/css" href="www/css/style1.css" />
 </head>
 
 <body>
+<div id="interface">
     <?php include_once "www/template/menu.php" ?>
     
     <div class='row'/>
     
     <div class='col-12 teste conteudo'>
-        fefefefe
+        <img src="www/imagens/catdog.png">
+        <h1>O que ele tem?</h1>
     </div>
     
     <?php include_once "www/template/footer.php" ?>
+</div>
+
 </body>
 
 </html>
